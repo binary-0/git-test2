12f6ec667ef11775445d4cee462ba921158a8612
bianry-0 <didi668@naver.com>
2021-03-15 20:57:55 +0900

    added sci score

--- report_card.c
+++ report_card.c
@@ -5,10 +5,12 @@ int main()
 	int korean = 90;
 	int math = 80;
 	int english = 70;
+	int sci = 60;
 
 	printf("%d\n", korean);
 	printf("%d\n", math);
 	printf("%d\n", english);
+	printf("%d\n", sci);
 
 	return 0;
 }
