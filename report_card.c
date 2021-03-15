742e2984d651bf0be7cbfabe72cd39091e1c18de
bianry-0 <didi668@naver.com>
2021-03-15 20:42:28 +0900

    report card: Add base code

---
+++ report_card.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+	int korean = 90;
+	int math = 80;
+	int english = 70;
+
+	printf("%d\n", korean);
+	printf("%d\n", math);
+	printf("%d\n", english);
+
+	return 0;
+}
