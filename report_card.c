dfb1362aa841c46215ec0f0691281564a752bcd0
bianry-0 <didi668@naver.com>
2021-03-15 21:01:37 +0900

    add soc score

--- report_card.c
+++ report_card.c
@@ -5,10 +5,12 @@ int main()
 	int korean = 90;
 	int math = 80;
 	int english = 70;
+	int society = 65;
 
 	printf("%d\n", korean);
 	printf("%d\n", math);
 	printf("%d\n", english);
+	printf("%d\n", society);
 
 	return 0;
 }
